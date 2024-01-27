f756580f7b4279b465dc678d28da105e7852dcd6
Brian Ngamsom <setthasiri.ngm@gmail.com>
2024-01-27 14:16:21 +1100

    update fallback port

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
